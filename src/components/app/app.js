ce67301411c810a1d1cf903e65fe1310c9f4d59f
nikita <honeyqumo@gmail.com>
2022-03-06 00:00:08 +0300

    v0.1.0 | Внешний вид приложения. Добавил компоненты shop-header, shopping-cart-table

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
+
+import ShopHeader from '../shop-header';
 import CartPage from '../pages/cart-page';
 import HomePage from '../pages/home-page';
 
@@ -8,10 +10,13 @@ import './app.css';
 const App = () => {
 
     return (
-        <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/cart' element={<CartPage />} />
-        </Routes>
+        <main role='main' className='container'>
+            <ShopHeader numItems={5} total={210} />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/cart' element={<CartPage />} />
+            </Routes>
+        </main>
     );
 };
 

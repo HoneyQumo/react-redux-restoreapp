65c8228376c2532aacb5de63535f4ea2fdd7271a
nikita <honeyqumo@gmail.com>
2022-03-10 09:04:49 +0300

    v0.1.8 | Релизовал добавление книги в корзину.

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,21 +3,24 @@ import { connect } from 'react-redux';
 
 
 import Spinner from '../spinner';
-import { fetchBooks } from '../../actions';
+import { fetchBooks, bookAddedToCart } from '../../actions';
 import BookListItem from '../book-list-item';
 import { withBookstoreService } from '../hoc';
 import { compose } from '../../utils';
 import './book-list.css';
 import ErrorIndicator from '../error-indicator';
 
-const BookList = ({ books }) => {
+const BookList = ({ books, onAddedToCart }) => {
     return (
         <ul className='book-list'>
             {
                 books.map((book) => {
                     return (
                         <li key={book.id}>
-                            <BookListItem book={book} />
+                            <BookListItem
+                                book={book}
+                                onAddedToCart={() => onAddedToCart(book.id)}
+                            />
                         </li>
                     )
                 })
@@ -33,7 +36,7 @@ class BookListContainer extends Component {
     }
 
     render() {
-        const { books, loading, error } = this.props;
+        const { books, loading, error, onAddedToCart } = this.props;
 
         if (loading) {
             return <Spinner />
@@ -43,7 +46,7 @@ class BookListContainer extends Component {
             return <ErrorIndicator />
         }
 
-        return <BookList books={books} />
+        return <BookList books={books} onAddedToCart={onAddedToCart} />
     }
 }
 
@@ -53,7 +56,8 @@ const mapStateToProps = ({ books, loading, error }) => {
 
 const mapDispatchToProps = (dispatch, { bookstoreService }) => {
     return {
-        fetchBooks: fetchBooks(bookstoreService, dispatch)
+        fetchBooks: fetchBooks(bookstoreService, dispatch),
+        onAddedToCart: (id) => dispatch(bookAddedToCart(id))
     }
 };
 

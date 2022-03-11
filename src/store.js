67b495edb55e60be39eb03c87224cf949b530ac8
nikita <honeyqumo@gmail.com>
2022-03-11 10:06:35 +0300

    v0.1.11 | Middleware. Использовал Enhancer 'applyMiddleware' из библиотеки redux

--- src/store.js
+++ src/store.js
@@ -1,35 +1,24 @@
-import { createStore, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import reducer from "./reducers";
 
-const logEnhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
+const logMiddleware = ({ getState }) => (next) => (action) => {
+    console.log(action.type, getState());
+    next(action);
+}
 
-    const originalDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        console.log(action.type);
-        originalDispatch(action);
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        });
     }
-    return store;
-};
 
-const stringEnhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
+    return next(action);
+}
 
-    const originalDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return originalDispatch({
-                type: action
-            });
-        }
 
-        originalDispatch(action);
-    }
-    return store;
-};
-
-const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
 
 store.dispatch('HELLO_WORLD');
 
